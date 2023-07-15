2258d8f52bb0346afe6c4e14cd789f45f6e380a6
MosasaUnited <mostafa447@hotmail.com>
2023-07-15 23:33:01 +0300

    WrapWithAMaterialDesign

--- lib/screens/explore_collection_card.dart
+++ lib/screens/explore_collection_card.dart
@@ -23,84 +23,87 @@ class ExploreCollectionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var themeData = Theme.of(context);
-    return ButtonTapEffect(
-      onTap: onTap,
-      borderRadius: 14,
-      margin: EdgeInsets.only(right: MySpaceSystem.spaceX3, top: 4, bottom: 4),
-      child: Container(
-        clipBehavior: Clip.hardEdge,
-        decoration: ShapeDecoration(
-          color: themeData.colorScheme.secondary,
-          shadows: cardShadow,
-          shape: SmoothRectangleBorder(
-            side: BorderSide(
-              width: 1.5,
-              color: themeData.colorScheme.outline,
-            ),
-            borderRadius: SmoothBorderRadius(
-              cornerRadius: 12,
-              cornerSmoothing: 1,
+    return Scaffold(
+      body: ButtonTapEffect(
+        onTap: onTap,
+        borderRadius: 14,
+        margin:
+            EdgeInsets.only(right: MySpaceSystem.spaceX3, top: 4, bottom: 4),
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: ShapeDecoration(
+            color: themeData.colorScheme.secondary,
+            shadows: cardShadow,
+            shape: SmoothRectangleBorder(
+              side: BorderSide(
+                width: 1.5,
+                color: themeData.colorScheme.outline,
+              ),
+              borderRadius: SmoothBorderRadius(
+                cornerRadius: 12,
+                cornerSmoothing: 1,
+              ),
             ),
           ),
-        ),
-        child: Stack(
-          children: [
-            // Big First Letter
-            Positioned(
-              right: MySpaceSystem.spaceX2,
-              top: 0,
-              child: Text(
-                title[0].toString(),
-                style: TextStyle(
-                  fontSize: 144,
-                  height: 1,
-                  color: themeData.colorScheme.outline,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-            ).animate().fadeIn(delay: 300.ms),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // title and Categories count
-                Padding(
-                  padding: EdgeInsets.all(MySpaceSystem.spaceX2 + 4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              title,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: themeData.textTheme.titleLarge,
-                            ),
-                            const SizedBox(
-                              height: MySpaceSystem.spaceX,
-                            ),
-                            Text(
-                              bodyText,
-                              style: themeData.textTheme.bodyMedium!
-                                  .copyWith(fontWeight: FontWeight.w700),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
+          child: Stack(
+            children: [
+              // Big First Letter
+              Positioned(
+                right: MySpaceSystem.spaceX2,
+                top: 0,
+                child: Text(
+                  title[0].toString(),
+                  style: TextStyle(
+                    fontSize: 144,
+                    height: 1,
+                    color: themeData.colorScheme.outline,
+                    fontWeight: FontWeight.w900,
                   ),
-                )
-                    .animate(delay: 200.ms)
-                    .moveX(begin: -100)
-                    .then()
-                    .shakeX(hz: 4, amount: 3),
-              ],
-            )
-          ],
+                ),
+              ).animate().fadeIn(delay: 300.ms),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // title and Categories count
+                  Padding(
+                    padding: EdgeInsets.all(MySpaceSystem.spaceX2 + 4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                title,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: themeData.textTheme.titleLarge,
+                              ),
+                              const SizedBox(
+                                height: MySpaceSystem.spaceX,
+                              ),
+                              Text(
+                                bodyText,
+                                style: themeData.textTheme.bodyMedium!
+                                    .copyWith(fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                      .animate(delay: 200.ms)
+                      .moveX(begin: -100)
+                      .then()
+                      .shakeX(hz: 4, amount: 3),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
